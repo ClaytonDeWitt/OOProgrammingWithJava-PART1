80f980834b1dbab2860c63d5eac898532ca4901f
Clayton <cdnp44@gmail.com>
2017-03-17 01:23:38 -0400

    Mooc.fi Java Course Part 1

---
+++ week1-005.SecondsInYear/src/SecondsInAYear.java
@@ -0,0 +1,17 @@
+
+public class SecondsInAYear {
+
+    public static void main (String [] args){
+
+        int daysInYear = 365;
+        int hoursInDay = 24;
+        int minutesInHour = 60;
+        int secondsInMinute = 60;
+
+        int secondsinYear = daysInYear * hoursInDay * minutesInHour * secondsInMinute;
+
+        System.out.println("THere are " + secondsinYear + " seconds in a year.");
+        
+
+    }
+}
