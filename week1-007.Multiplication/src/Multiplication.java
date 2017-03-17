e54673f235caf6b537f32f899ce473e61a80ed83
Clayton <cdnp44@gmail.com>
2017-03-17 01:48:04 -0400

    Mooc.fi Java Course Part 1

--- week1-007.Multiplication/src/Multiplication.java
+++ week1-007.Multiplication/src/Multiplication.java
@@ -1,15 +1,16 @@
-public class Addition {
+public class Multiplication {
 
     public static void main(String[] args) {
 
         int a = 1337;
         int b = 42;
-        int result = a + b; // Fix this
 
-        String toPrint = a + " + " + b + " = " + result;
+        int result = a * b;
+
+        String toPrint =  a  + " * " + b  + " = " + result;
         System.out.println(toPrint);
+
 
     }
 
 }
-   
