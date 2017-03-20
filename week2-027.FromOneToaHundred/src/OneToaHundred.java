e107f4146ec5e652ede0f3886a70ba7cb7dae951
Clayton <cdnp44@gmail.com>
2017-03-19 23:28:58 -0400

    Mooc.fi Java Course Part 1

---
+++ week2-027.FromOneToaHundred/src/OneToaHundred.java
@@ -0,0 +1,16 @@
+
+public class OneToaHundred {
+
+    public static void main (String[] args){
+
+        int num = 1;
+
+        while (num <= 100){
+            System.out.println(num);
+            num++;
+        }
+
+    }
+}
+
+
